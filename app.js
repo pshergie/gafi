a5936bd38edfa0a139807a6a1381943931eafa05
Maksim Minko <dethroll@gmail.com>
2020-06-06 22:50:30 +0300

    add some comments & improve logic

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
+// импорт библиотек
 const express = require('express');
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const app = express();
-app.engine('pug', require('pug').__express)
-const url = 'https://money.cnn.com/data/fear-and-greed/';
-const selector = '#needleChart ul li:first-child';
+const app = express(); // инициализируем express (что это? https://expressjs.com/ru/)
+const url = 'https://money.cnn.com/data/fear-and-greed/'; // адрес нужного сайта
+const selector = '#needleChart ul li:first-child'; // необходимый селектор на странице
 
+// вызываем функцию, которая делает основную работу
 fetch(url)
   .then(res => res.text())
   .then(html => {
     const $ = cheerio.load(html);
     const data = $(selector);
     const index = data.text().match(/\d+/)[0];
-    const today  = new Date();
+    const today = new Date();
+    const date = today.toLocaleDateString('en-GB', { timeZone: 'UTC' })
 
     console.log(`Fetched index: ${index}`);
 
-    const record = { 
+    const newRecord = { 
       index: index,
-      date: today.toLocaleDateString('en-GB', { timeZone: 'UTC' }),
+      date: date,
     };
 
     fs.readFile('data.json', function (err, data) {
       if (err) throw err;
       const json = JSON.parse(data);
-      json.push(record);
-  
-      fs.writeFileSync('data.json', JSON.stringify(json));
+
+      if (json[json.length - 1].date !== date) {
+        json.push(newRecord);
+        fs.writeFileSync('data.json', JSON.stringify(json));
+      }
 
       app.get('/', function (req, res) {
         res.render('main.pug', { data: JSON.stringify(json) });
       });
       
       app.listen(3000, function () {
-        console.log('Example app listening on port 3000!');
+        console.log('App listening on port 3000');
       });
     });
   }).catch((e) => {
